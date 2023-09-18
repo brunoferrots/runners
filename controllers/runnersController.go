69537b7888ac6d1c52e068f7f3651e8e6da5ebf4
brunoferrots <brunoferrots@outlook.com>
2023-09-18 20:19:42 -0300

    FEATURE: ...step 3 - implementation of controllers layers

--- controllers/runnersController.go
+++ controllers/runnersController.go
@@ -23,23 +23,61 @@ func NewRunnersController(runnersService *service.runnersService) *RunnersContro
 func (rh RunnersController) CreateRunner(ctx *gin.Context) {
 	body, err := io.ReadAll(ctx.Request.Body)
 	if err != nil {
-		log.Println("Error while reading runners request body", err)
+		log.Println("Error while reading create runners request body", err)
 		ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
+
 	var runner models.Runner
 	err = json.Unmarshal(body, &runner)
 	if err != nil {
-
+		log.Println("Erro while unmarshaling update runners request body", err)
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
 	}
+
+	response, responseErr := rh.runnersService.CreateRunner(&runner)
+	if responseErr != nil {
+		ctx.AbortWithStatusJSON(responseErr.Status, responseErr)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
 
 func (rh RunnersController) UpdateRunner(ctx *gin.Context) {
+	body, err := io.ReadAll(ctx.Request.Body)
+	if err != nil {
+		log.Println("Error while reading update runners request body", err)
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
 
+	var runner models.Runner
+	err = json.Unmarshal(body, &runner)
+	if err != nil {
+		log.Println("Erro while unmarshaling update runners request body", err)
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	response, reponseErr := rh.runnersService.UpdateRunner(&runner)
+	if reponseErr != nil {
+		ctx.AbortWithStatusJSON(reponseErr.Status, reponseErr)
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, response)
 }
 
 func (rh RunnersController) DeleteRunner(ctx *gin.Context) {
-
+	runnerId := ctx.Param("id")
+	responseErr := rh.runnersService.DeleteRunner(runnerId)
+	if responseErr != nil {
+		ctx.AbortWithStatusJSON(responseErr.Status, responseErr)
+		return
+	}
+	ctx.Status(http.StatusNoContent)
 }
 
 func (rh RunnersController) GetRunner(ctx *gin.Context) {
